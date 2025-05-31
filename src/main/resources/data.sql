ba7113a2176a4b5cc1bf47431712aae361aab8f8
Brandon Arias Cjuno <brandon12arias@gmail.com>
2025-05-31 11:32:39 -0500

    Data base

---
+++ src/main/resources/data.sql
@@ -0,0 +1,52 @@
+// Comando utiles
+DELETE FROM "user";
+TRUNCATE TABLE "user";
+DROP TABLE "user";
+
+
+
+//Base de datos
+-- Crear tabla de roles
+CREATE TABLE rol (
+     id_rol SERIAL PRIMARY KEY,
+     denominacion VARCHAR(50) NOT NULL,
+     estado BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+-- Insertar roles
+INSERT INTO rol (denominacion) VALUES
+   ('alumno'),         -- id_rol = 1
+   ('moderador'),      -- id_rol = 2
+   ('administrador');  -- id_rol = 3
+
+-- Crear tabla de usuarios
+CREATE TABLE "user" (
+    id_usuario SERIAL PRIMARY KEY,
+    correo_institucional VARCHAR(100) NOT NULL UNIQUE,
+    contraseña VARCHAR(100) NOT NULL,
+    user_rol INTEGER NOT NULL,
+    FOREIGN KEY (user_rol) REFERENCES rol(id_rol)
+);
+
+-- Insertar usuarios con rol de alumno (id_rol = 1)
+INSERT INTO "user" (correo_institucional, contraseña, user_rol) VALUES
+    ('juan.perez@universidad.edu', 'clave123', 1),
+    ('maria.lopez@universidad.edu', 'segura456', 1),
+    ('carlos.gomez@universidad.edu', 'pass789', 1);
+
+CREATE TABLE historial_preferencias (
+    id_historial_preferencias SERIAL PRIMARY KEY,
+    id_usuario INT NOT NULL,
+    contenido TEXT,
+    FOREIGN KEY (id_usuario) REFERENCES "user"(id_usuario)
+);
+INSERT INTO historial_preferencias (id_usuario, contenido) VALUES
+   (1, 'libros de ciencia ficción y fantasía'),
+   (1, 'contenido audiovisual sobre documentales'),
+   (2, 'temas de desarrollo personal y liderazgo'),
+   (2, 'recomendaciones de libros de negocios'),
+   (3, 'podcasts sobre historia mundial'),
+   (3, 'música instrumental relajante'),
+   (1, 'contenido sobre inteligencia artificial'),
+   (2, 'foros sobre filosofía'),
+   (3, 'cursos de programación');
