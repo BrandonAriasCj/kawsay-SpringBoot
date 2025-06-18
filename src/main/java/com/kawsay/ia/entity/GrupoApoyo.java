30ba460e0f70d25faf3dc67f8dea3a418453e147
Brandon Arias Cjuno <brandon12arias@gmail.com>
2025-06-18 10:10:24 -0500

    primer avance de tablas grupo apoyo y publicacion

--- src/main/java/com/kawsay/ia/entity/GrupoApoyo.java
+++ src/main/java/com/kawsay/ia/entity/GrupoApoyo.java
@@ -1,4 +1,20 @@
 package com.kawsay.ia.entity;
 
+import jakarta.persistence.*;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="grupo_apoyo")
 public class GrupoApoyo {
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String descripcion;
+    private LocalDateTime fechaCreacion;
+    private Boolean estado;
 }
--- src/main/java/com/kawsay/ia/entity/Publicacion.java
+++ src/main/java/com/kawsay/ia/entity/Publicacion.java
@@ -1,4 +1,17 @@
 package com.kawsay.ia.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="publicacion")
 public class Publicacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    private String titulo;
+    private String contenido;
+    private String imagen;
+
 }
