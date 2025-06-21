cd05feee3cf309b1529ccd212deb7ae24c2f06cf
Brandon Arias Cjuno <brandon12arias@gmail.com>
2025-06-21 11:28:53 -0500

    modificaciones, Navbar más dinamico

--- src/front-end/src/components/Navbar.jsx
+++ src/front-end/src/components/Navbar.jsx
@@ -1,14 +1,36 @@
 // src/components/Navbar.jsx
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext.jsx';
 import '../styles/Navbar.css';
 
 const Navbar = () => {
   const { user } = useContext(AuthContext);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      // Mostrar si el puntero está en los primeros 50px verticales
+      if (e.clientY < 80) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+
+
+
 
   return (
-    <nav className="navbar">
+    <nav className={`main-header ${isVisible ? 'show' : 'hide'} navbar`}>
       <h1>KawzAi</h1>
       <div className="links">
         <Link to="/">Inicio</Link>
