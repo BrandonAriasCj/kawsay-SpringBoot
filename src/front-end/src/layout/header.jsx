32a34f747524f47a86794b4461732878325294f6
Brandon Arias Cjuno <brandon12arias@gmail.com>
2025-06-20 22:11:36 -0500

    componente header

---
+++ src/front-end/src/layout/header.jsx
@@ -0,0 +1,11 @@
+const header = ()=>{
+
+    return (
+              <div className="chat-header">
+                <img src="" alt="KawsAI Logo" className="chat-logo" />
+                KawsAI - Historias Interactivas
+              </div>
+    );   
+}
+
+export default header
