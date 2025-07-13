16883433510e6e6657c0e337d6371b38fa00dd5f
Brandon Arias Cjuno <brandon12arias@gmail.com>
2025-07-13 02:33:00 -0500

    chages

--- src/front-end/src/utils/path.js
+++ src/front-end/src/utils/path.js
@@ -1,8 +1,12 @@
     const ip = import.meta.env.IP_PUBLICA;
-    const puerto_back = import.meta.env.PUERTO_BACK;
+/*     const puerto_back = import.meta.env.PUERTO_BACK;
     const puerto_admin = import .meta.env.PUERTO_FRONT_ADMIN;
     const puerto_user = import .meta.env.PUERTO_FRONT_USER;
-    const protocolo = import.meta.env.PROTOCOLO;
+    const protocolo = import.meta.env.PROTOCOLO; */
+    const puerto_back = 8081;
+    const puerto_admin = 3001;
+    const puerto_user = 3000;
+    const protocolo = `http`;
 
     export const urlBaseBack =  ()=>{
         const rutaBase = `${protocolo}://${ip}:${puerto_back}`;
